792684e826b86d3f1defce137a63e74cf0dd9df4
singhal-shubham <85708182+singhal-shubham@users.noreply.github.com>
2021-06-18 22:29:42 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 
 // Getting uplayer names
-p1 = prompt('Enter Player 1 name');
-p2 = prompt('Enter Player 2 name');
+var p1 = prompt('Enter Player 1 name');
+var p2 = prompt('Enter Player 2 name');
 
 // Displaying plaer names
 document.querySelectorAll(".dice p")[0].textContent = p1;
 document.querySelectorAll(".dice p")[1].textContent = p2;
 
-// Generating the dice outcomes
-var d1 = 1 + Math.floor(Math.random()*6);
-var d2 = 1 + Math.floor(Math.random()*6);
+document.addEventListener("onload", function() {
 
-document.querySelector("img.img1").setAttribute("src", "images/dice" + d1 + ".png");
-document.querySelector("img.img2").setAttribute("src", "images/dice" + d2 + ".png");
+  diceRoll(p1, p2);
 
-if (d1 > d2) {
-  document.querySelector(".container h1").innerHTML = "<img class='winner' src='images/winner.png'>" + p1 + " wins"
-} else if(d2 > d1) {
-  document.querySelector(".container h1").innerHTML = "<img class='winner' src='images/winner.png'>" + p2 + " wins"
-} else {
-  document.querySelector(".container h1").innerHTML = "Draw!"
+});
+
+
+function diceRoll(p1, p2) {
+
+  // Generating the dice outcomes
+  var d1 = 1 + Math.floor(Math.random()*6);
+  var d2 = 1 + Math.floor(Math.random()*6);
+
+  document.querySelector("img.img1").setAttribute("src", "images/dice" + d1 + ".png");
+  document.querySelector("img.img2").setAttribute("src", "images/dice" + d2 + ".png");
+
+  if (d1 > d2) {
+    document.querySelector(".container h1").innerHTML = "<img class='winner' src='images/winner.png'>" + p1 + " wins";
+  } else if(d2 > d1) {
+    document.querySelector(".container h1").innerHTML = "<img class='winner' src='images/winner.png'>" + p2 + " wins";
+  } else {
+    document.querySelector(".container h1").innerHTML = "Draw!";
+  }
+
 }
+
+
+
+document.querySelector(".btn").addEventListener("click", function() {
+  diceRoll(p1, p2);
+});
